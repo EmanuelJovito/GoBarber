92af0b5cadc9cd748550990b6c7adc8995a4379f
Emanuel Jovito <emanuel_carneiro@hotmail.com>
2021-02-04 11:21:33 -0300

    WIP: Backend da aplicação

---
+++ backend/src/routes/appointments.routes.ts
@@ -0,0 +1,25 @@
+import {Router} from 'express'
+import {startOfHour, parseISO} from 'date-fns'
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository'
+
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.post('/', (request, response) => {
+  const {provider, date} = request.body;
+
+  const parsedDate = startOfHour(parseISO(date))
+
+  const findAppointmentInSameDate =  appointmentsRepository.findByDate(parsedDate)
+
+  if (findAppointmentInSameDate) {
+    return response.status(400).json({message: "This appointment is already booked"})
+  }
+
+  const appointment = appointmentsRepository.create(provider, parsedDate)
+
+  return response.json(appointment)
+})
+
+export default appointmentsRouter;
