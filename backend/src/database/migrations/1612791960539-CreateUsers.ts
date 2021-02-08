edd31d603380d818f551e6a20c72faf21b5e7e71
Emanuel Jovito <emanuel_carneiro@hotmail.com>
2021-02-08 10:56:39 -0300

    feat: Criando migration de usuario

--- backend/src/database/migrations/1612791960539-CreateUsers.ts
+++ backend/src/database/migrations/1612791960539-CreateUsers.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateAppointments1612533070521 implements MigrationInterface {
+export default class CreateUsers1612791960539 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -14,12 +14,23 @@ export class CreateAppointments1612533070521 implements MigrationInterface {
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
           },
         ],
       }),
@@ -27,6 +38,6 @@ export class CreateAppointments1612533070521 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
