ce1a6010b51eabe5c3cb0452b818af24fce0c068
Emanuel Jovito <emanuel_carneiro@hotmail.com>
2021-02-09 09:25:46 -0300

    feat: Criando relacionamento de models

--- backend/src/database/migrations/1612533070521-CreateAppointments.ts
+++ backend/src/database/migrations/1612533070521-CreateAppointments.ts
@@ -8,7 +8,7 @@ export class CreateAppointments1612533070521 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
