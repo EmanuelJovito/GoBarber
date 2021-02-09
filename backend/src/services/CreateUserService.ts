6c31de5b0bc7af725a0e5c4df29be0bcaa983af2
Emanuel Jovito <emanuel_carneiro@hotmail.com>
2021-02-09 10:59:36 -0300

    feat: Criando registro

---
+++ backend/src/services/CreateUserService.ts
@@ -0,0 +1,35 @@
+import { getRepository } from 'typeorm';
+
+import User from '../models/User';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const checkUserExist = await usersRepository.findOne({
+      where: { email },
+    });
+
+    if (checkUserExist) {
+      throw Error('Email address already used');
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
