b108bbc535604286ac2c4e0e22057fc9c9bc2bd5
Emanuel Jovito <emanuel_carneiro@hotmail.com>
2021-02-10 11:22:50 -0300

    feat: Criando atualização de avatar

---
+++ backend/src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,40 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+
+import uploadConfig from '../config/upload';
+import User from '../models/User';
+
+interface Request {
+  userId: string;
+  avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ userId, avatarFilename }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne(userId);
+
+    if (!user) {
+      throw new Error('Only authenticated users can change avatar');
+    }
+
+    if (user.avatar) {
+      const UserAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const UserAvatarFileExists = await fs.promises.stat(UserAvatarFilePath);
+
+      if (UserAvatarFileExists) {
+        await fs.promises.unlink(UserAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFilename;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
