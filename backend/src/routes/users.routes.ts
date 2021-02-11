770fc2bcff93857e315deb5fb4d384e9f9dc35db
Emanuel Jovito <emanuel_carneiro@hotmail.com>
2021-02-11 09:04:15 -0300

    feat: Criando tratativas de error

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -46,20 +46,16 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
+    const updateUserAvatar = new UpdateUserAvatarService();
 
-      const user: User = await updateUserAvatar.execute({
-        userId: request.user.id,
-        avatarFilename: request.file.filename,
-      });
+    const user: User = await updateUserAvatar.execute({
+      userId: request.user.id,
+      avatarFilename: request.file.filename,
+    });
 
-      delete user.password;
+    delete user.password;
 
-      return response.json(user);
-    } catch (err) {
-      return response.status(400).json({ error: err.message });
-    }
+    return response.json(user);
   },
 );
 
