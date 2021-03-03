b2e225620fe1c944c21c08e1e5b38703a1a33d59
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-03-03 10:33:32 -0300

    feat: Criando estrutura de Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import SignIn from './pages/SignIn';
 import GlobalStyle from './styles/global';
 
+import ToastContainer from './components/ToastContainer';
+
 import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => {
@@ -11,6 +13,8 @@ const App: React.FC = () => {
       <AuthProvider>
         <SignIn />
       </AuthProvider>
+
+      <ToastContainer />
 
       <GlobalStyle />
     </>
--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -45,9 +45,11 @@ const SignIn: React.FC = () => {
           password: data.password,
         });
       } catch (err) {
-        const errors = getValidationErrors(err);
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
 
-        formRef.current?.setErrors(errors);
+          formRef.current?.setErrors(errors);
+        }
       }
     },
     [signIn],
